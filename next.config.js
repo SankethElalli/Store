f0e01de75623c45f4f31f1d51f660d973431722e
SankethElalli <elallisanketh7@gmail.com>
2025-03-15 11:03:57 +0530

    deploy changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: '/Store',
+  assetPrefix: '/Store/',
   output: 'export',
-  // ...existing code...
 };
+
+export default nextConfig;
--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  basePath: '/Store',
-  assetPrefix: '/Store/',
-};
-
-export default nextConfig;
